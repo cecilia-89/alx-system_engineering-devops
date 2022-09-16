96f8982ce8ecebe581e0ebae0a6c7da7e767b64a
cecilia-89 <catabong89@gmail.com>
2022-09-16 19:55:41 +0100

    installed: flask via puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Create a file called school
+# Create a file in /tmp
 
 file {  'school':
-    path  => '/tmp/school',
+  content => 'I love Puppet',
+    group => 'www-data',
     mode  => '0744',
     owner => 'www-data',
-    group => 'www-data',
-  content => 'I love Puppet',
+    path  => '/tmp/school',
 }
