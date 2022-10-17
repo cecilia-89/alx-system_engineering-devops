e88538226f51a7805ead5e972e7d183e17544bff
cecilia-89 <catabong89@gmail.com>
2022-10-17 17:40:27 +0100

    initial commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,13 +4,14 @@ from sys import argv
 import requests
 
 
-url = "https://jsonplaceholder.typicode.com/users/"
-user = requests.get(url + "{}".format(argv[1])).json()
-todos = requests.get(url + "{}/todos".format(argv[1])).json()
-tasks = [todo.get('title') for todo in todos if todo.get('completed') is True]
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/"
+    user = requests.get(url + "{}".format(argv[1])).json()
+    todos = requests.get(url + "{}/todos".format(argv[1])).json()
+    tasks = [todo.get('title') for todo in todos if todo.get('completed') is True]
 
-print("Employee {} is done with tasks({}/{}):"
-      .format(user.get('name'), len(tasks), len(todos)))
+    print("Employee {} is done with tasks({}/{}):"
+        .format(user.get('name'), len(tasks), len(todos)))
 
-for task in tasks:
-    print("\t{}".format(task))
+    for task in tasks:
+        print("\t{}".format(task))
