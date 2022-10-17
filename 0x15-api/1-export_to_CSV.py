e0712bc12fa5feaea367a50412a24b6694acb544
cecilia-89 <catabong89@gmail.com>
2022-10-17 16:53:43 +0100

    initial commit

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""returns information about an employee's TODO list progress"""
+import requests
+from sys import argv
+import csv
+
+url = "https://jsonplaceholder.typicode.com/users/"
+
+user = requests.get(url + "{}".format(argv[1])).json()
+todos = requests.get(url + "{}/todos".format(argv[1])).json()
+
+with open("{}.csv".format(user.get('id')), "w+") as f:
+	file = csv.writer(f, quoting=csv.QUOTE_ALL)
+	for todo in todos:
+		file.writerow([user.get('id'), user.get('username'),
+		             todo.get('completed'), todo.get('title')])
---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""returns information about an employee's TODO list progress"""
+import requests
+from sys import argv
+import json
+
+url = "https://jsonplaceholder.typicode.com/users/"
+
+user = requests.get(url + "{}".format(argv[1])).json()
+todos = requests.get(url + "{}/todos".format(argv[1])).json()
+dic = {}
+lst = []
+
+for todo in todos:
+    dic['task'] = todo.get('title')
+    dic['completed'] = todo.get('completed')
+    dic['username'] = user.get('username')
+    lst.append(dic)
+
+my_dict = {user.get('id'): lst}
+
+
+with open("{}.json".format(user.get('id')), "w+") as f:
+    json.dump(my_dict, f)
