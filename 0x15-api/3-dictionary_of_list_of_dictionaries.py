2b5b11032ea246d20573f6b1d412448c702126bc
cecilia-89 <catabong89@gmail.com>
2022-10-17 20:18:07 +0100

    initial commit

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,14 +6,14 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
-    todos = requests.get(url + "todos").json()
-
+    url = "https://jsonplaceholder.typicode.com/users/"
+    users = requests.get(url).json()
 
     with open("todo_all_employees.json", "w+") as f:
         json.dump({user.get('id'): [{
                   "task": todo.get('title'),
                   "completed": todo.get('completed'),
                   "username": user.get('username')}
-                  for todo in todos] for user in users}, f)
+                  for todo in requests.get(url + "{}/todos"\
+                  .format(user.get('id'))).json()]
+                  for user in users}, f)
